742b9c43b63fda3f51a358b517e5a879b87bb8eb
Thomah <5090230+Thomah@users.noreply.github.com>
2020-11-13 13:23:23 +0100

    Bump @angular/core to 11.0.0

--- src/app/pages/news/view-news/view-news.page.spec.ts
+++ src/app/pages/news/view-news/view-news.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ViewNewsPage } from './view-news.page';
@@ -7,7 +7,7 @@ describe('ViewNewsPage', () => {
   let component: ViewNewsPage;
   let fixture: ComponentFixture<ViewNewsPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ViewNewsPage ],
       imports: [IonicModule.forRoot()]
