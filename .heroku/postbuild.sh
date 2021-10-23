eebf72724bf06d297906d885305977286a5966f4
Thomah <5090230+Thomah@users.noreply.github.com>
2021-10-23 20:47:59 +0200

    Prepare migration to Heroku

---
+++ .heroku/postbuild.sh
@@ -0,0 +1,2 @@
+ng build -- --configuration production-fr --output-path=dist
+cp .heroku/config.json dist/assets/config.json
--- src/environments/environment.heroku.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  webUrl: 'https://les-sagas-mp3.fr.herokuapp.com',
-  apiUrl: 'https://les-sagas-mp3-api.herokuapp.com/api'
-};
