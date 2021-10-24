37b6981c33591d21e0d27a068edc65ef27c20b01
Thomah <5090230+Thomah@users.noreply.github.com>
2021-10-24 18:49:26 +0200

    Add variables to Heroku deployment

--- .heroku/postbuild.sh
+++ .heroku/postbuild.sh
@@ -1,3 +1,8 @@
 ng build --configuration=production-fr --output-path=dist
+if [ -z $APP_URL ] || [ -z $API_URL ];
+then
+    export APP_URL="https://les-sagas-mp3-app-staging.herokuapp.com/"
+    export API_URL="https://les-sagas-mp3-api-staging.herokuapp.com/api"
+fi
+echo "{\"webUrl\":\"$APP_URL\",\"apiUrl\":\"$API_URL\"}" > .heroku/config.json
 cp .heroku/config.json dist/assets/config.json
-printenv | more
