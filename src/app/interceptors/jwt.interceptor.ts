f33954f85d515f120585674912fa0dae0379c336
Thomah <5090230+Thomah@users.noreply.github.com>
2023-01-07 20:30:32 +0100

    fix: call to authService.currentTokenValue

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -11,7 +11,7 @@ export class JwtInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let jwtResponse = this.authService.currentTokenValue;
-        if (jwtResponse && jwtResponse.token) {
+        if (jwtResponse && jwtResponse.token != '') {
             request = request.clone({
                 setHeaders: { 
                     Authorization: `Bearer ${jwtResponse.token}`
